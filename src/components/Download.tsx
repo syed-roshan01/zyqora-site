ed4d42d4b6d930ed6f003afae3bacc61902b57df
syed roshan <toroshaninbox1@gmail.com>
2026-03-15 05:15:49 +0530

    Update Windows download URL to v1.0.1

--- src/components/Download.tsx
+++ src/components/Download.tsx
@@ -89,8 +89,8 @@ export default function Download() {
 
   const handleWindowsDownload = () => {
     const link = document.createElement('a');
-    link.href = 'https://github.com/syed-roshan01/zyqora-site/releases/download/v1.0.0/Zyqora.Setup.1.0.0.exe';
-    link.download = 'Zyqora Setup 1.0.0.exe';
+    link.href = 'https://github.com/syed-roshan01/zyqora-site/releases/download/v1.0.1/Zyqora.Setup.1.0.1.exe';
+    link.download = 'Zyqora Setup 1.0.1.exe';
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
